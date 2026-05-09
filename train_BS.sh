6709d44908bfefb00fc3afe9d1010aee9dbb3ba1
gracnewt <156862430+gracnewt@users.noreply.github.com>
2026-05-09 02:09:47 -0400

    slurm bertBS

---
+++ train_BS.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#SBATCH --job-name=BSBert
+#SBATCH --output=result.out
+#SBATCH --nodes=1
+#SBATCH --gres=gpu:1        # Request a GPU for your BERT model
+#SBATCH --mem=32G
+#SBATCH --nodelist=student-gpu-002
+
+# 1. Force the correct environment
+source /home/gracenewton/miniconda3/etc/profile.d/conda.sh
+conda activate legal_nlp
+
+# 2. Set Paths - DOUBLE CHECK THESE!
+export BILLSUM_PREFIX="/home/gracenewton/nlp_final/BillSum/billsum/data/"
+export PYTHONPATH=$PYTHONPATH:.
+
+# Stop script if any command fails
+set -e
+
+#echo "Starting Clean Text..."
+#python -u billsum/data_prep/clean_text.py
+
+echo "Starting Sentence Labeling..."
+python -u billsum/data_prep/label_sentences_BS.py
+
+echo "Starting BERT_BS Training..."
+python -u bert_full_train_BS.py
